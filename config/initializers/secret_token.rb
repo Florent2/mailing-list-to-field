a6e9ab318fc98f40cb6d2a661c5e8b5017f3d07d
Florent Guilleux <florent2@gmail.com>
2012-10-12 09:49:36 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MailingListToField::Application.config.secret_token = '138dfbc9b67a7f1da6d9ac9d54eabde4811e26c2551fc5519a1732f66b1680d109f616f7bcd909e26e1b271659d8d2d08db2367a09191dc71e7dafdb3d3fcd14'
